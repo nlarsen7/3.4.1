a19f05e306bd48e7eb68c9f1aeaf718f4ba5bba1
nlarsen7 <nlarsen2013@gmail.com>
2020-04-25 20:29:33 +0000

    Lab 3.4.1

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/25/2020
+//Creates a function that asks the user's age and name and outputs it
 package main
 
 import "fmt"
@@ -5,12 +8,16 @@ import "fmt"
 func age() {
 var user string
 var age int
+//variables for age and user name
 fmt.Println("What is you name?")
 fmt.Scanln(&user)
 fmt.Println("what is your age in years?")
 fmt.Scanln(&age)
+//asks for user name and age and stores in variable
 fmt.Println("Hello",user,"you are",age,"years old")
+//Prints out the name and age
 }
 func main (){
   age()
+  //another function that does what the age function does
 }
